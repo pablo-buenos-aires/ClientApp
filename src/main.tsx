20f42768168e081cdc30b4cc474069444fadd742
pablo-buenos-aires <pp3842879@gmail.com>
2025-11-16 00:48:19 -0300

    15/11

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,12 @@ import App from './App'
 import {BrowserRouter} from "react-router-dom";
 
 import { AuthProvider, AuthProviderProps } from "react-oidc-context";
-
+//redirect_uri: "https://localhost:44407/auth/callback",
 const cognitoAuthConfig: AuthProviderProps = {
   authority: "https://cognito-idp.sa-east-1.amazonaws.com/sa-east-1_abyVMc2Px",
   client_id: "1dthdfdnlojrvd2c56663dvo86",
-  redirect_uri: "https://localhost:44407/auth/callback",
+   // redirect_uri: "https://localhost:44407/auth/callback",
+     redirect_uri: "https://d1i4ngjfyhcuut.cloudfront.net/auth/callback",
   response_type: "code", // именно code, не token
   scope: "email openid",
   // можно добавить опцию для отображения login-страницы вместо signup:
