0de5ff344ef4a0d49e187ee7e8adbbd7f2187499
pablo-buenos-aires <pp3842879@gmail.com>
2025-12-11 20:47:52 -0300

    55

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -26,7 +26,7 @@ function IndexPage() {
     const [error, setError] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
 
-    const API_BASE = 'backend-alb-635489014.sa-east-1.elb.amazonaws.com/api';
+    const API_BASE = 'http://backend-alb-635489014.sa-east-1.elb.amazonaws.com/api';
 
     // Загрузка профиля при монтировании компонента
     useEffect(() => {
